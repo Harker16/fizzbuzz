8872c3a66aa8323f443e65e400db9faa1d9b3408
Thomas Harker <tom.harker@hotmail.co.uk>
2016-05-20 19:08:01 +0000

    Add requirement that the number is divisible by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,9 @@
 require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+    it 'returns "fizz" when passed a number that is divisible by 3' do
+        expect(fizzbuzz(6)).to eq 'fizz'
+        expect(fizzbuzz(30)).to eq 'fizz'
+    end
+  
 end
